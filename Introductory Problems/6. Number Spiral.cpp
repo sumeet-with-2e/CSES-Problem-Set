373f78dea98ff32b0f0dd1262f2761530b9a8922
Sumeet Pachauri <kspa4275@gmail.com>
2022-01-17 02:33:46 +0530

    Adding Solutions to Problems 5, 6, 7

---
+++ Introductory Problems/6. Number Spiral.cpp
@@ -0,0 +1,59 @@
+/*
+Author : Sumeet Pachauri
+Codeforces : sumeet_4275
+CodeChef : kaalbhairav01 
+________________________________________
+█─▄▄▄▄█▄─██─▄█▄─▀█▀─▄█▄─▄▄─█▄─▄▄─█─▄─▄─█
+█▄▄▄▄─██─██─█#█─█▄█─█#█─▄█▀██─▄█▀███─███
+▀▄▄▄▄▄▀▀▄▄▄▄▀▀▄▄▄▀▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▀▄▄▄▀▀
+*/
+ 
+#include "bits/stdc++.h"
+using namespace std;
+#define int               long long
+#define pb                push_back
+#define ppb               pop_back
+#define pf                push_front
+#define ppf               pop_front
+#define fastio			  ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define all(x)            (x).begin(),(x).end()
+#define uniq(v)           (v).erase(unique(all(v)),(v).end())
+#define sz(x)             (int)((x).size())
+#define fr                first
+#define sc                second
+#define pii               pair<int,int>
+#define rep(i,a,b)        for(int i=a;i<b;i++)
+#define mem1(a)           memset(a,-1,sizeof(a))
+#define mem0(a)           memset(a,0,sizeof(a))
+#define r0                return 0
+#define test			  int T; cin>>T; while(T--)
+#define ppc               __builtin_popcount
+#define ppcll             __builtin_popcountll
+#define mod				  1000000007
+ 
+void solve()
+{ 	int x, y;
+	cin>>y>>x;
+	int ans;
+	if(y>=x)
+	{
+		if(y%2==0)
+			ans = (y*y) - (x - 1);
+		else 
+			ans = ((y-1)*(y-1)+1) + x - 1;
+	}else
+	{
+		if(x%2!=0)
+			ans = (x*x) - (y - 1);
+		else
+			ans = ((x-1)*(x-1)+1) + y - 1;
+	}
+	cout<<ans<<"\n";
+} 
+ 
+signed main(){
+	fastio
+	test
+	solve();
+	r0;
+}
