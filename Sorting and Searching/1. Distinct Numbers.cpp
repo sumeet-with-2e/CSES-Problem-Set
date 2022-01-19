f4fb0c6c3cb656620ee1abb8f7d0092b0e85c3f5
Sumeet Pachauri <kspa4275@gmail.com>
2022-01-19 23:28:51 +0530

    Sorting and Searching first question

---
+++ Sorting and Searching/1. Distinct Numbers.cpp
@@ -0,0 +1,46 @@
+/*
+Author : Sumeet Pachauri
+Codeforces : sumeet_4275
+CodeChef : kaalbhairav01 
+________________________________________
+█─▄▄▄▄█▄─██─▄█▄─▀█▀─▄█▄─▄▄─█▄─▄▄─█─▄─▄─█
+█▄▄▄▄─██─██─█#█─█▄█─█#█─▄█▀██─▄█▀███─███
+▀▄▄▄▄▄▀▀▄▄▄▄▀▀▄▄▄▀▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▀▄▄▄▀▀
+*/
+ 
+#include "bits/stdc++.h"
+using namespace std;
+#define int               long long
+#define pb                push_back
+#define ppb               pop_back
+#define pf                push_front
+#define ppf               pop_front
+#define fastio			  ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define all(x)            (x).begin(),(x).end()
+#define uniq(v)           (v).erase(unique(all(v)),(v).end())
+#define sz(x)             (int)((x).size())
+#define fr                first
+#define sc                second
+#define pii               pair<int,int>
+#define rep(i,a,b)        for(int i=a;i<b;i++)
+#define mem1(a)           memset(a,-1,sizeof(a))
+#define mem0(a)           memset(a,0,sizeof(a))
+#define r0                return 0
+#define test			  int T; cin>>T; while(T--)
+#define ppc               __builtin_popcount
+#define ppcll             __builtin_popcountll
+#define mod				  1000000007
+ 
+void solve()
+{   int n;
+    cin>>n;
+    set<int> v;
+    rep(i,0,n){int temp; cin>>temp; v.insert(temp);}
+    cout<<v.size()<<"\n";
+}
+signed main(){
+	fastio
+//	test
+	solve();
+	r0;
+}
